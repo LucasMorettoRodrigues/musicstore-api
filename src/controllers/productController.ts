8b5a41d0138171b63049b629a56fef08f5cb2216
Lucas <r.lucasmoretto@gmail.com>
2022-03-14 18:41:30 -0300

    clean code

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -3,7 +3,7 @@ const Product = require('../models/Product')
 
 export const getProduct = async (req: Request, res: Response) => {
     try {
-        const product = await Product.findOne({_id: req.params.id})
+        const product = await Product.findOne({ _id: req.params.id })
         res.status(200).json(product)
     } catch (error) {
         res.status(500).json(error)
@@ -14,23 +14,24 @@ export const getProducts = async (req: Request, res: Response) => {
 
     type queryType = {
         "category"?: string,
-        "name"?: string
+        "name"?: object
     }
 
     const queryObject: queryType = {}
 
     const { category, name } = req.query
 
-    if(typeof category === "string") {
+    if (typeof category === "string") {
         queryObject.category = category
     }
 
-    if(name) {
+    if (name) {
         queryObject.name = { $regex: name, $options: 'i' }
     }
 
     try {
         const products = await Product.find(queryObject)
+
         res.status(200).json(products)
     } catch (error) {
         res.status(500).json(error)
